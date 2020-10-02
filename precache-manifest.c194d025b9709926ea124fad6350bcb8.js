b9e8d9f878bc40152bf8b0cdbf6825a82885c819
Benjamin Loschen <loschenbd@gmail.com>
2020-10-02 14:22:33 -0500

    Updates

---
+++ precache-manifest.c194d025b9709926ea124fad6350bcb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b997309af4e9ea9f5f3214d90a36001",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "0a20704236f6b959fb8c",
+    "url": "/react-calculator/static/css/main.a91245b7.chunk.css"
+  },
+  {
+    "revision": "e5a486b8a3e41b0f21ca",
+    "url": "/react-calculator/static/js/2.29a33ec0.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.29a33ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a20704236f6b959fb8c",
+    "url": "/react-calculator/static/js/main.7370a14f.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
